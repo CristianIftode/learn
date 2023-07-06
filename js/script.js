15f70770b995bd51e801f86d139be898bd0d2b12
Sapphire <iftode2014@yandex.ru>
2023-07-06 22:42:21 +0300

    webpack

---
+++ js/script.js
@@ -0,0 +1,17 @@
+import tabs from './modules/tabs.js';
+import timer from './modules/timer.js';
+import modal from './modules/modal.js';
+import forms from './modules/forms.js';
+import slider from './modules/slider.js';
+import cards from './modules/cards.js';
+import calc from './modules/calc.js';
+
+window.addEventListener('DOMContentLoaded', function() {
+    tabs();
+    timer();
+    modal();
+    forms();
+    slider();
+    cards();
+    calc();
+});
